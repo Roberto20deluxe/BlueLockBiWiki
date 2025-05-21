f8558e4b6ec7c1f606ad47c5a48e4197350080e7
Breno Augusto Ferreira <brenoafsa@gmail.com>
2025-05-21 00:25:43 -0300

    feat(database-creation-modeling)
    created the user model and configured the prisma ORM for the application

---
+++ script.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "./generated/prisma"
+const prisma = new PrismaClient()
+
+async function main(){
+    const users = await prisma.user.findMany()
+    console.log(users)
+}
+
+main()
+
+.catch(e => {
+    console.error(e.message)
+})
+.finally(async () => {
+    await prisma.$disconnect()
+})
