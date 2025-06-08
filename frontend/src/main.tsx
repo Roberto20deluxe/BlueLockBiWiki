2c8ee3723e2fd363544c4d81511f1bf63475836f
Breno Augusto Ferreira <brenoafsa@gmail.com>
2025-06-08 18:31:08 -0300

    refactor(create-player-modal)
    substitute all the JS code for TS in the Modal Page and also adds some validation for some of the POST values, changes the main page to be HomePage again

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import HomePage from './pages/HomePage'
-import CharacterPage from './pages/CharacterPage'
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <CharacterPage/>
+      <HomePage/>
     </StrictMode>
   );
 } else {
