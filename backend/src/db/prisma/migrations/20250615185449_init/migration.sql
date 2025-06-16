e6876a60a0b83165cd530d8932cede30540fbe54
Breno Augusto Ferreira <155557831+brenoafsa@users.noreply.github.com>
2025-06-15 21:24:46 -0300

    feat(application-dockerization)
    moves database folder to inside backend's folder, reorganizes frontend, backend and database folders to fit a better repository architecture, integrates dockerfiles and docker-compose.yml to allow containers

---
+++ backend/src/db/prisma/migrations/20250615185449_init/migration.sql
@@ -0,0 +1,59 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "favBLPlayer" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BLPLayer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "nickname" TEXT NOT NULL,
+    "ageComment" TEXT,
+    "birthDate" TIMESTAMP(3) NOT NULL,
+    "birthPlace" TEXT NOT NULL,
+    "height" DOUBLE PRECISION NOT NULL,
+    "domLeg" TEXT NOT NULL,
+    "shoeSize" DOUBLE PRECISION NOT NULL,
+    "bloodType" TEXT NOT NULL,
+    "vision" DOUBLE PRECISION NOT NULL,
+    "zodiac" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+
+    CONSTRAINT "BLPLayer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "orderNumber" INTEGER NOT NULL,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Response" (
+    "id" TEXT NOT NULL,
+    "playerId" TEXT NOT NULL,
+    "questionId" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "comment" TEXT,
+
+    CONSTRAINT "Response_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Response_playerId_questionId_key" ON "Response"("playerId", "questionId");
+
+-- AddForeignKey
+ALTER TABLE "Response" ADD CONSTRAINT "Response_playerId_fkey" FOREIGN KEY ("playerId") REFERENCES "BLPLayer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Response" ADD CONSTRAINT "Response_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
